ba8dbbe1becb507d78d375f8e76fbaba90899123
Alan Sproat <alan@sproatcentral.com>
2024-05-01 21:59:02 -0500

    create project

---
+++ whoswinningredux/whoswinningredux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whoswinningredux
+//
+//  Created by Alan Sproat on 5/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whoswinningredux/whoswinningredux/whoswinningreduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  whoswinningreduxApp.swift
+//  whoswinningredux
+//
+//  Created by Alan Sproat on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct whoswinningreduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
